7633ecfcf2a1c1528a6d599e406ec910e079c146
Sean Grogan <sean.grogan@gmail.com>
2022-08-31 12:51:46 -0400

    V1.3.0 update: Modified __error_checking.py to throw errors by default.

--- build/lib/great_circle_calculator/__init__.py
+++
@@ -1,1 +0,0 @@
-name = "great_circle_calculator"
--- great_circle_calculator/__error_checking.py
+++ great_circle_calculator/__error_checking.py
@@ -1,16 +1,32 @@
-def _error_check_point(point, correct_point=True):
+import warnings
+
+
+def _error_check_point(point, correct_point=False):
     if len(point) != 2:
-        print("Point", str(point), "is incorrect length!")
-        return False
+        raise TypeError(f"Point {str(point)} is incorrect length!")
     lon, lat = float(point[0]), float(point[1])
     if -90 <= lat <= 90 and -180 <= lon <= 180:  # Point makes sense
         return (lon, lat)
     elif -90 <= lon <= 90 and -180 <= lat <= 180:  # The point is (probably!) reversed
-        print("Point", str(point), "is probably reversed!")
+        _msg = f"Point {str(point)} is probably reversed!\n\n" \
+               f"We believe that this is the case because\n" \
+               f"the provided longitude ({lon}) is in the range of [-90, 90]\n" \
+               f"and the provided latitude ({lat}) is in the range of [-180, 180].\n\n" \
+               f"The pont must be provided as a tuple of length 2, where the\n" \
+               f"first element is the longitude in the range of [-180, 180], and\n" \
+               f"the second element is the latitude in the range of [-90, 90].\n"
+
         if correct_point:
+            warnings.warn(_msg)
+            warnings.warn(f"The parameter 'correct_point' has been set to True,\n"
+                          f"Therefore we are reversing the point and continuing...")
             point_corrected = (lat, lon)
             return point_corrected
-        return False
+        else:
+            raise ValueError(_msg)
     else:
-        print("Point", str(point), "Cannot be interpreted!")
-        return False
+        raise ValueError(f"\n"
+                         f"Point {str(point)} cannot be appropriately interpreted in this program.\n"
+                         f"Be advised, the pont must be provided as a tuple of length 2, where the\n"
+                         f"first element is the longitude in the range of [-180, 180], and\n"
+                         f"the second element is the latitude in the range of [-90, 90]).\n")
