514b646043eec1b30ea3d2e5705f86acc5f80603
sean grogan <sean.grogan@gmail.com>
2026-09-15 17:41:43 -0400

    Added annotations to the files (tbh, was in most files)

--- great_circle_calculator/__conversion.py
+++ great_circle_calculator/__conversion.py
@@ -1,27 +1,23 @@
-from great_circle_calculator._constants import *
-from great_circle_calculator.__error_checking import _error_check_point
+from __future__ import annotations
+
+from great_circle_calculator._constants import pi
+from great_circle_calculator.__error_checking import Point, _error_check_point
 
 
-def _degrees_to_radians(degrees):
-    """ Converts the degrees into radians
-    :param degrees: decimal degrees
-    :return: radians
-    """
-    return pi * degrees / 180
+def _degrees_to_radians(degrees: float) -> float:
+    """Converts degrees into radians."""
+    return pi * degrees / 180.0
 
 
-def _radians_to_degrees(radians):
-    """ Converts the radians into degrees
-    :param radians: decimal degrees
-    :return: radians
-    """
-    return 180 * radians / pi
+def _radians_to_degrees(radians: float) -> float:
+    """Converts radians into degrees."""
+    return 180.0 * radians / pi
 
 
-def _point_to_radians(point):
+def _point_to_radians(point: Point) -> Point:
     point = _error_check_point(point)
-    return (_degrees_to_radians(point[0]), _degrees_to_radians(point[1]))
+    return _degrees_to_radians(point[0]), _degrees_to_radians(point[1])
 
 
-def _point_to_degrees(point):
-    return (_radians_to_degrees(point[0]), _radians_to_degrees(point[1]))
+def _point_to_degrees(point: Point) -> Point:
+    return _radians_to_degrees(point[0]), _radians_to_degrees(point[1])
