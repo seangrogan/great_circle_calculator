7621a00f584f7dbae27d32b0e3880e96ca694207
sean grogan <sean.grogan@gmail.com>
2026-09-15 17:39:35 -0400

    Updated __error_checking.py, fixing typos and adding a function to deal with a shapely Point

--- great_circle_calculator/__error_checking.py
+++ great_circle_calculator/__error_checking.py
@@ -1,32 +1,59 @@
+from __future__ import annotations
+
 import warnings
 
 
-def _error_check_point(point, correct_point=False):
+Point = tuple[float, float]
+
+
+def _handle_point(obj):
+    """
+    Function used to process if a passed "point" is a shapely Point type/class
+
+    The program ultimately will use native python types for returning
+    """
+
+    if hasattr(obj, "__geo_interface__"):
+        geo = obj.__geo_interface__
+        if geo["type"] != "Point":
+            raise TypeError(f"Expected a Point, got {geo['type']}")
+        x, y = geo["coordinates"][:2]
+    elif isinstance(obj, (tuple, list)) and len(obj) >= 2:
+        x, y = obj[0], obj[1]
+    else:
+        raise TypeError(f"Cannot interpret {type(obj)} as a point")
+    return x, y
+
+def _error_check_point(point: Point, correct_point: bool = False) -> Point:
+    point = _handle_point(point)
     if len(point) != 2:
-        raise TypeError(f"Point {str(point)} is incorrect length!")
+        raise TypeError(f"Point {point!s} is incorrect length!")
     lon, lat = float(point[0]), float(point[1])
     if -90 <= lat <= 90 and -180 <= lon <= 180:  # Point makes sense
-        return (lon, lat)
+        return lon, lat
     elif -90 <= lon <= 90 and -180 <= lat <= 180:  # The point is (probably!) reversed
-        _msg = f"Point {str(point)} is probably reversed!\n\n" \
-               f"We believe that this is the case because\n" \
-               f"the provided longitude ({lon}) is in the range of [-90, 90]\n" \
-               f"and the provided latitude ({lat}) is in the range of [-180, 180].\n\n" \
-               f"The pont must be provided as a tuple of length 2, where the\n" \
-               f"first element is the longitude in the range of [-180, 180], and\n" \
-               f"the second element is the latitude in the range of [-90, 90].\n"
-
+        _msg = (
+            f"Point {point!s} is probably reversed!\n\n"
+            f"We believe that this is the case because\n"
+            f"the provided longitude ({lon}) is in the range of [-90, 90]\n"
+            f"and the provided latitude ({lat}) is in the range of [-180, 180].\n\n"
+            f"The point must be provided as an iterable or tuple of length 2, where the\n"
+            f"first element is the longitude in the range of [-180, 180], and\n"
+            f"the second element is the latitude in the range of [-90, 90].\n"
+        )
         if correct_point:
             warnings.warn(_msg)
-            warnings.warn(f"The parameter 'correct_point' has been set to True,\n"
-                          f"Therefore we are reversing the point and continuing...")
-            point_corrected = (lat, lon)
-            return point_corrected
-        else:
-            raise ValueError(_msg)
+            warnings.warn(
+                "The parameter 'correct_point' has been set to True,\n"
+                "Therefore we are reversing the point and continuing..."
+            )
+            return lat, lon
+        raise ValueError(_msg)
     else:
-        raise ValueError(f"\n"
-                         f"Point {str(point)} cannot be appropriately interpreted in this program.\n"
-                         f"Be advised, the pont must be provided as a tuple of length 2, where the\n"
-                         f"first element is the longitude in the range of [-180, 180], and\n"
-                         f"the second element is the latitude in the range of [-90, 90]).\n")
+        raise ValueError(
+            f"\n"
+            f"Point {point!s} cannot be appropriately interpreted in this program.\n"
+            f"Be advised, the point must be provided as a tuple of length 2, where the\n"
+            f"first element is the longitude in the range of [-180, 180], and\n"
+            f"the second element is the latitude in the range of [-90, 90]).\n"
+        )
