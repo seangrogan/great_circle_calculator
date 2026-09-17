51d436b514a1e6c9ab617a09da653bbbbb813114
sean grogan <sean.grogan@gmail.com>
2026-09-16 21:57:58 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="great_circle_calculator",
-    version="1.3.1",
+    version="2.0.0",
     author="Sean Grogan",
     author_email="sean.grogan@gmail.com",
     description="A collection of functions to calculate attributes of the great circle",
@@ -13,13 +13,15 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/seangrogan/great_circle_calculator",
     packages=setuptools.find_packages(),
+    python_requires=">=3.9",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
